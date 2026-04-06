7e910ae8888a60e9dbf4016ed71cea107cdeabe1
gustavooo <gusdominguez09@gmail.com>
2026-04-06 11:22:05 -0500

    Commit a los problemas de la practica

---
+++ Problema1.php
@@ -0,0 +1,43 @@
+<?php
+// suma
+$numero1 = 5;
+$numero2 = 3;
+$resultado = $numero1 + $numero2;
+//resultado=8
+echo "suma = $resultado";
+
+//resta
+$numero1 = 10;
+$numero2 = 4;
+$resultado = $numero1 - $numero2;
+//resultado=6
+echo "\nresta = $resultado";
+
+//multiplicacion
+$numero1 = 7;
+$numero2 = 3;
+$resultado = $numero1 * $numero2;
+//resultado=21
+echo "\nmultiplicación = $resultado";
+
+//division
+$numero1 = 15;
+$numero2 = 5;
+$resultado = $numero1 / $numero2;
+//resultado=3
+echo "\ndivisión = $resultado";
+
+//modulo
+$numero1 = 10;
+$numero2 = 3;
+$resultado = $numero1 % $numero2;
+//resultado=1
+echo "\nmodulo = $resultado";
+
+//potencia
+$numero1 = 2;
+$numero2 = 3;
+$resultado= $numero1 ** $numero2;
+//resultado=8
+echo "\npotencia= $resultado";
+?>
---
+++ Problema2.php
@@ -0,0 +1,9 @@
+<?php
+$resultado = 2 + 3 * 4;
+//resultado=14
+echo "resultado 1 = $resultado\n";
+
+$resultado = (2 + 3) * 4;
+//resultado=20
+echo "resultado 2 = $resultado";
+?>
